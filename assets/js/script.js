07afb96ab1e5fe2488ba2aefdc5275a6f89ba000
sehaj97 <sehajmagan@gmail.com>
2021-09-18 11:03:58 -0400

    updated the delete btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector("#page-content");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
@@ -82,5 +83,17 @@ var createTaskEl = function(taskDataObj) {
 
     return actionContainerEl;
 };
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+      }
+  };
 
+  var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+pageContentEl.addEventListener("click", taskButtonHandler);
 formEl.addEventListener("submit", taskFormHandler);
